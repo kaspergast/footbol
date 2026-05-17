e6e281c46727bf18205fd68907000a7bf0e37325
camilo <galvisherreracamilo@gmail.com>
2026-05-16 21:44:54 -0500

    Agrego archivo con el diseño basico sin funcionalidades

---
+++ Futbol/src/components/Menu.jsx
@@ -0,0 +1,19 @@
+import './menue.css'
+function Menu1() {
+  return (
+    <>
+      <nav className="menu1">
+        <div className="nav_menu">
+          <p>top club soccer</p>
+          <h2>Dashboard de jugadores</h2>
+          <p>gestiona tus favoritas, analiza estadisticas y descubre talentos</p>
+        </div>
+        <div className="boton_action">
+          <button className="boton_de_cambio"><p>cambio de tema</p></button>
+        </div>
+
+      </nav>
+    </>
+  )
+}
+export default Menu1
